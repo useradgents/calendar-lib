fcaadece3fe5b4a330846a6f92bb9c3f4685a65c
rcheze <r.cheze@useradgents.com>
2018-10-01 15:50:11 +0200

    1.1.14 - Init date

--- calendarlib/src/main/java/com/useradgents/calendarlib/controller/CalendarSingleController.kt
+++ calendarlib/src/main/java/com/useradgents/calendarlib/controller/CalendarSingleController.kt
@@ -4,7 +4,9 @@ import java.util.*
 
 class CalendarSingleController constructor(private val calendarView: CalendarViewInterface) : CalendarControllerInterface {
 
-    override fun setSelectedDates(dates: Pair<Date?, Date?>) {}
+    override fun setSelectedDates(dates: Pair<Date?, Date?>) {
+        dates.first?.let { calendarView.onFirstDateSet(it) }
+    }
 
     override var dateList: List<Date>? = null
 
